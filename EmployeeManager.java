b70f379fad0404bed62e2e72078f4283fb0a0570
shahariar-hridoy <shahariark79@gmail.com>
2025-11-21 20:45:52 +0600

    Task #1.    Updated Code Style for Better Consistency

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -1,4 +1,3 @@
-//File Name EmployeeManager.java
 
 import java.io.*;
 import java.util.*;
@@ -8,7 +7,9 @@ public class EmployeeManager {
     public static void main(String[] args) {
         // Check arguments
         if (args[0].equals("l")) {
+
             System.out.println("Loading data ...");
+
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
@@ -18,37 +19,55 @@ public class EmployeeManager {
                 for (String emp : e) {
                     System.out.println(emp);
                 }
+
             } catch (Exception e) {
+
             }
+
             System.out.println("Data Loaded.");
+
         } else if (args[0].equals("s")) {
+
             System.out.println("Loading data ...");
+            
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
                                 new FileInputStream("employees.txt")));
                 String l = r.readLine();
-               
+
                 String e[] = l.split(",");
                 Random rand = new Random();
                 int idx = rand.nextInt(e.length);
                 System.out.println(e[idx]);
+
             } catch (Exception e) {
+                
             }
+
             System.out.println("Data Loaded.");
+
         } else if (args[0].startsWith("+")) {
+
             System.out.println("Loading data ...");
+
             try {
                 BufferedWriter w = new BufferedWriter(
                         new FileWriter("employees.txt", true));
                 String n = args[0].substring(1);
                 w.write("," + n);
                 w.close();
+
             } catch (Exception e) {
+
             }
+
             System.out.println("Data Loaded.");
+
         } else if (args[0].startsWith("?")) {
+
             System.out.println("Loading data ...");
+
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
@@ -57,17 +76,25 @@ public class EmployeeManager {
                 String e[] = l.split(",");
                 boolean found = false;
                 String s = args[0].substring(1);
+
                 for (int i = 0; i < e.length && !found; i++) {
+
                     if (e[i].equals(s)) {
                         System.out.println("Employee found!");
                         found = true;
                     }
                 }
+
             } catch (Exception e) {
+
             }
+
             System.out.println("Data Loaded.");
+
         } else if (args[0].contains("c")) {
+
             System.out.println("Loading data ...");
+
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
@@ -76,6 +103,7 @@ public class EmployeeManager {
                 char[] chars = l.toCharArray();
                 boolean inWord = false;
                 int count = 0;
+
                 for (char c : chars) {
                     if (c == ' ') {
                         if (!inWord) {
@@ -86,12 +114,19 @@ public class EmployeeManager {
                         }
                     }
                 }
+
                 System.out.println(count + " word(s) found " + chars.length);
+                
             } catch (Exception e) {
+
             }
+
             System.out.println("Data Loaded.");
+
         } else if (args[0].startsWith("u")) {
+
             System.out.println("Loading data ...");
+
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
@@ -99,7 +134,9 @@ public class EmployeeManager {
                 String l = r.readLine();
                 String e[] = l.split(",");
                 String n = args[0].substring(1);
+
                 for (int i = 0; i < e.length; i++) {
+
                     if (e[i].equals(n)) {
                         e[i] = "Updated";
                     }
@@ -108,11 +145,17 @@ public class EmployeeManager {
                         new FileWriter("employees.txt"));
                 w.write(String.join(",", e));
                 w.close();
+
             } catch (Exception e) {
+
             }
+
             System.out.println("Data Updated.");
+
         } else if (args[0].contains("d")) {
+
             System.out.println("Loading data ...");
+
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
@@ -126,9 +169,13 @@ public class EmployeeManager {
                         new FileWriter("employees.txt"));
                 w.write(String.join(",", list));
                 w.close();
+
             } catch (Exception e) {
+
             }
+
             System.out.println("Data Deleted.");
+            
         }
     }
 }
